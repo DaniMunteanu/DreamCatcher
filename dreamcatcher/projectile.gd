230ef1e8962dd7dc8c966165b331e7fd8ae104bc
Dani Munteanu <93090044+DaniMunteanu@users.noreply.github.com>
2025-01-22 15:46:06 +0200

    Delete "dreamcatcher" directory to sync

--- dreamcatcher/projectile.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-var speed = 700
-
-var mouse_location = Vector2()
-
-func _physics_process(delta):
-	var shooting_direction = mouse_location
-	global_position -= shooting_direction * speed * delta
