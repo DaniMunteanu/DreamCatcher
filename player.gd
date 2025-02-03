07b71a860f5717c2d53c9fef6de8282a927f087e
Dani Munteanu <daniel.munteanu@student.upt.ro>
2025-02-03 03:27:00 +0200

    Fixed jumping distance

--- player.gd
+++ player.gd
@@ -10,6 +10,13 @@ enum player_states {MOVE, JUMP, FIRE}
 var current_state = player_states.MOVE
 
 var jumping_direction = Vector2.ZERO
+var jumping_target_position = Vector2.ZERO
+
+var jumping_distance = 96.0
+
+func _ready():
+	jumping_distance = global_position.distance_to($"%MaxJumpMarker".global_position)
+	print(jumping_distance)
 
 func update_movement():
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -19,7 +26,9 @@ func update_movement():
 	
 func update_sprite():
 	if Input.is_action_pressed("evade") :
-		jumping_direction = get_global_position().direction_to(get_global_mouse_position())
+		jumping_target_position = global_position.move_toward(get_global_mouse_position(), jumping_distance)
+		jumping_direction = global_position.direction_to(jumping_target_position).normalized()
+	
 		current_state = player_states.JUMP
 		
 	if Input.is_action_just_pressed("fire"):
@@ -41,15 +50,17 @@ func update_sprite():
 	else:
 		animation_player.play_idle_right_animation()
 
-func jump(jumping_direction):
+func jump(delta):
 	animation_player.play_jumping_right_animation()
 	
-	velocity = jumping_direction * 97.0
-	move_and_slide()
+	if global_position.distance_to(jumping_target_position) > 3:
+		global_position += global_position.direction_to(jumping_target_position) * speed * delta
 	
 func fire(delta):
 	if Input.is_action_pressed("evade") :
-		jumping_direction = get_global_position().direction_to(get_global_mouse_position())
+		jumping_target_position = global_position.move_toward(get_global_mouse_position(), jumping_distance)
+		jumping_direction = global_position.direction_to(jumping_target_position).normalized()
+		
 		current_state = player_states.JUMP
 	
 	weapon.fire(delta)
@@ -87,7 +98,7 @@ func _physics_process(delta):
 			update_sprite()
 			get_node("JumpMarkerAnimation").play("JumpReady")
 		player_states.JUMP: 
-			jump(jumping_direction)
+			jump(delta)
 		player_states.FIRE:
 			update_movement()
 			fire(delta)
