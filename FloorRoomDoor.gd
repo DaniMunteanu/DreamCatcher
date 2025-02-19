2bf4ccbb197144381a1f817b12af9723fe5e9e1e
Dani Munteanu <daniel.munteanu@student.upt.ro>
2025-02-19 02:50:01 +0200

    Finished room and door collisions

--- FloorRoomDoor.gd
+++ FloorRoomDoor.gd
@@ -2,6 +2,13 @@ class_name FloorRoomDoor
 
 extends Node2D
 
+var room_1 = 0
+var room_2 = 0
+
+func set_rooms(room_1_index: int, room_2_index: int):
+	room_1 = room_1_index
+	room_2 = room_2_index
+	
 func open_door():
 	get_node("AnimationPlayer").play("Open")
 	
