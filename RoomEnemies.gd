7c653440a1c7e2bc6c3938086979aba733c5d312
Dani Munteanu <daniel.munteanu@student.upt.ro>
2025-04-16 03:29:22 +0300

    Fixed Y-sort of floor walls

--- RoomEnemies.gd
+++ RoomEnemies.gd
@@ -5,3 +5,14 @@ extends Node2D
 @export var enemies_count: int
 
 signal enemies_defeated
+
+func _ready() -> void:
+	enemies_count = get_child_count()
+	Global.enemy_dead.connect(_on_enemy_dead)
+
+func _on_enemy_dead(enemy_death_position: Vector2):
+	enemies_count -= 1
+	if enemies_count == 0:
+		print("Au disparut inamicii")
+		enemies_defeated.emit()
+		queue_free()
--- room_enemies/room_2_0.gd
+++ room_enemies/room_2_0.gd
@@ -1,12 +1,1 @@
 extends RoomEnemies
-
-func _ready() -> void:
-	enemies_count = 2
-	Global.enemy_dead.connect(_on_enemy_dead)
-
-func _on_enemy_dead(enemy_death_position: Vector2):
-	enemies_count -= 1
-	if enemies_count == 0:
-		print("Au disparut inamicii")
-		enemies_defeated.emit()
-		queue_free()
