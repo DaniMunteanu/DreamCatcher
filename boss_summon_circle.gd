855687cea687cd600f05b0bf17c48c11bae97ff4
Dani Munteanu <daniel.munteanu@student.upt.ro>
2025-05-09 04:57:56 +0300

    Added screen fade to black effect

--- boss_summon_circle.gd
+++ boss_summon_circle.gd
@@ -1,4 +1,13 @@
 extends Node2D
 
+@onready var player = get_parent().get_parent().get_node("Player")
+
 func _ready() -> void:
+	$InteractionArea.interact = Callable(self, "summon_boss")
+	
+func summon_boss():
+	$InteractionArea.can_interact = false
+	TransitionScreen.transition()
+	await TransitionScreen.on_transition_finished
+	player.global_position = $PlayerEndPosition.global_position
 	$AnimationPlayer.play("Summon")
--- interaction_area.gd
+++ interaction_area.gd
@@ -3,6 +3,9 @@ class_name InteractionArea
 extends Area2D
 
 @export var action_name: String = "interact"
+@export var label_text: String
+
+@onready var label = $Label
 
 #temporar
 var can_interact = false
@@ -11,18 +14,19 @@ var interact: Callable = func():
 	pass
 	
 func _ready() -> void:
-	get_node("Label").hide()
+	label.hide()
+	label.text = label_text
 
 func _on_body_entered(body: Node2D) -> void:
-	get_node("Label").show()
+	label.show()
 	can_interact = true
 
 func _on_body_exited(body: Node2D) -> void:
-	get_node("Label").hide()
+	label.hide()
 	can_interact = false
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") && can_interact:
 		interact.call()
-		get_node("Label").hide()
+		label.hide()
 		can_interact = false
