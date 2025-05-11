b64a009dd26314c120e3ec7e87422fd57de6f6d0
Dani Munteanu <daniel.munteanu@student.upt.ro>
2025-05-11 04:39:42 +0300

    Started adding rising walls

--- tothermos.gd
+++ tothermos.gd
@@ -1,11 +1,14 @@
 extends StaticBody2D
 
 @onready var BULLET = preload("res://BossBullet.tscn")
+@onready var player = get_parent().get_parent().get_node("Player")
 
 var bullets_start = []
 var bullets_end = []
 
 var bullet_lines = []
+
+enum boss_attacks {BULLETS_HALF, BULLETS_MINUS_ONE}
 
 func init_bullets_start():
 	bullets_start.resize(32)
@@ -32,9 +35,16 @@ func attack():
 
 func show_attack_indicators():
 	$AttackIndicatorsAnimations.play("BulletsIndication")
+	
+func prepare_bullets_minus_one():
+	var firing_angle = rad_to_deg(global_position.angle_to_point(player.global_position))
+	if firing_angle < 0.0:
+		firing_angle += 360.0
+	print(firing_angle)
 
 func _ready() -> void:
 	init_bullets_start()
 	init_bullets_end()
 	init_bullet_lines()
-	show_attack_indicators()
+	#show_attack_indicators()
+	prepare_bullets_minus_one()
